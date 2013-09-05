f7dfdd925595aa92162fd7123657a4ae1223a758
Adrian Goins <monachus@arces.net>
2013-09-05 18:24:56 -0400

    little tiny changes

--- run/run.sh
+++ run/run.sh
@@ -2,11 +2,11 @@
 
 CONTAINER="monachus/openfire"
 
-# Set this to "-d" to background the container.  
-DAEMON=""
+# Uncomment this to background the container
+DAEMON="-d"
 
 # File sharing proxy port
-#FSPROXY="-p 7777:7777"
+FSPROXY="-p 7777:7777"
 
 if [ ! -d $(pwd)/etc ] || [ ! -d $(pwd)/lib ]; then
     echo "Please run this from the 'run' directory to access the persistent volumes."
