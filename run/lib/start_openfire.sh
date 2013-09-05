754a033e4a95f551be59be7a47a09f923c835fc9
Adrian Goins <monachus@arces.net>
2013-09-05 15:37:20 -0400

    modified method for startup

--- run/lib/start_openfire.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-chown -R openfire:openfire /etc/openfire /var/lib/openfire
-sudo -u openfire -H /usr/lib/jvm/default-java/bin/java -server -DopenfireHome=/usr/share/openfire -Dopenfire.lib.dir=/usr/share/openfire/lib -classpath /usr/share/openfire/lib/startup.jar -jar /usr/share/openfire/lib/startup.jar
-
--- run/run.sh
+++ run/run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+CONTAINER="monachus/openfire"
+
+# Set this to "-d" to background the container.  
+DAEMON=""
+
 if [ ! -d $(pwd)/etc ] || [ ! -d $(pwd)/lib ]; then
     echo "Please run this from the 'run' directory to access the persistent volumes."
     exit 1
 fi
 
-docker run -d -v $(pwd)/lib:/var/lib/openfire -v $(pwd)/etc:/etc/openfire monachus/openfire
+docker run $DAEMON -v $(pwd)/lib:/var/lib/openfire -v $(pwd)/etc:/etc/openfire $CONTAINER
