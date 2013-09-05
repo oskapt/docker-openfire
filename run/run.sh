ef117b3a62405ed5d585669484f9cc1c01caf20f
Adrian Goins <monachus@arces.net>
2013-09-05 17:18:22 -0400

    fileserver proxy in options

--- run/run.sh
+++ run/run.sh
@@ -5,9 +5,12 @@ CONTAINER="monachus/openfire"
 # Set this to "-d" to background the container.  
 DAEMON=""
 
+# File sharing proxy port
+#FSPROXY="-p 7777:7777"
+
 if [ ! -d $(pwd)/etc ] || [ ! -d $(pwd)/lib ]; then
     echo "Please run this from the 'run' directory to access the persistent volumes."
     exit 1
 fi
 
-docker run $DAEMON -v $(pwd)/lib:/var/lib/openfire -v $(pwd)/etc:/etc/openfire $CONTAINER
+docker run $DAEMON $FSPROXY -v $(pwd)/lib:/var/lib/openfire -v $(pwd)/etc:/etc/openfire $CONTAINER
