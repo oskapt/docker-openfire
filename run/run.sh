a0350e883b90475f1d25ad1bf28101155f85418e
Adrian Goins <monachus@arces.net>
2013-11-26 16:20:44 -0300

    config updates for docker 0.7

--- run/run.sh
+++ run/run.sh
@@ -6,11 +6,11 @@ CONTAINER="monachus/openfire"
 DAEMON="-d"
 
 # File sharing proxy port
-FSPROXY="-p 7777:7777"
+PORTS="-p 7777:7777 -p 5222:5222 -p 9091:9091 -p 5269:5269 -p :9999"
 
 if [ ! -d $(pwd)/etc ] || [ ! -d $(pwd)/lib ]; then
     echo "Please run this from the 'run' directory to access the persistent volumes."
     exit 1
 fi
 
-docker run $DAEMON $FSPROXY -v $(pwd)/lib:/var/lib/openfire -v $(pwd)/etc:/etc/openfire $CONTAINER
+docker run $DAEMON $PORTS -name openfire -v $(pwd)/lib:/var/lib/openfire -v $(pwd)/etc:/etc/openfire $CONTAINER
